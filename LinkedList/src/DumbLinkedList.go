b89fb61a5f6dd387b25df3ca985e0c4e09aac49d
Jon Moroney <darakian@gmail.com>
2018-08-07 12:01:20 -1000

    Add remove functions

--- LinkedList/src/DumbLinkedList.go
+++ LinkedList/src/DumbLinkedList.go
@@ -18,6 +18,13 @@ func (l *DumbLinkedList) add_front(n *Node){
   l.length++
 }
 
+func (l *DumbLinkedList) remove_front() *Node{
+  tmp := l.Head
+  l.Head = l.Head.Next
+  l.length--
+  return tmp
+}
+
 func (l *DumbLinkedList) add_back(n *Node){
   current_pointer := l.Head
   for (current_pointer!=nil){
@@ -28,12 +35,26 @@ func (l *DumbLinkedList) add_back(n *Node){
   current_pointer.Next = n
 }
 
+func (l *DumbLinkedList) remove_back() *Node{
+  current_pointer := l.Head
+  previous_pointer := current_pointer
+  for (current_pointer!=nil){
+    if current_pointer.Next!=nil{
+      previous_pointer=current_pointer
+      current_pointer=current_pointer.Next
+    } else {break}
+  }
+  previous_pointer.Next=nil
+  return current_pointer
+}
+
 func (l DumbLinkedList) print_list(){
   current_pointer := l.Head
   for (current_pointer!=nil){
     fmt.Println(*current_pointer)
     current_pointer=current_pointer.Next
   }
+  fmt.Println("//-----//")
 }
 
 func main() {
@@ -44,5 +65,9 @@ func main() {
   dumb_list.add_front(&n1)
   dumb_list.add_front(&n2)
   dumb_list.add_back(&n3)
+  dumb_list.print_list()
+  dumb_list.remove_front()
+  dumb_list.print_list()
+  dumb_list.remove_back()
   dumb_list.print_list()
 }
