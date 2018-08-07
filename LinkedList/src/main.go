e99bd4e7acb949eb00215d433a699d29671fb90a
Jon Moroney <darakian@gmail.com>
2018-08-07 11:52:34 -1000

    Basic adding and printing

--- LinkedList/src/main.go
+++ LinkedList/src/main.go
@@ -7,11 +7,42 @@ type Node struct {
 	Next *Node
 }
 
+type DumbLinkedList struct {
+  length int
+  Head *Node
+}
+
+func (l *DumbLinkedList) add_front(n *Node){
+  n.Next = l.Head
+  l.Head = n
+  l.length++
+}
+
+func (l *DumbLinkedList) add_back(n *Node){
+  current_pointer := l.Head
+  for (current_pointer!=nil){
+    if current_pointer.Next!=nil{
+      current_pointer=current_pointer.Next
+    } else {break}
+  }
+  current_pointer.Next = n
+}
+
+func (l DumbLinkedList) print_list(){
+  current_pointer := l.Head
+  for (current_pointer!=nil){
+    fmt.Println(*current_pointer)
+    current_pointer=current_pointer.Next
+  }
+}
+
 func main() {
-	v := Node{1, nil}
-	p := &v
-	p.Value = 1e9
-	fmt.Println(v)
-	head := Node{0, p}
-	fmt.Println(*head.Next)
+  n1 := Node{1, nil}
+  n2 := Node{2, nil}
+  n3 := Node{3, nil}
+  dumb_list := DumbLinkedList{0, nil}
+  dumb_list.add_front(&n1)
+  dumb_list.add_front(&n2)
+  dumb_list.add_back(&n3)
+  dumb_list.print_list()
 }
